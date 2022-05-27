67ba2b153bc9b2b9546fe6f8fd392bc0e4b646e1
MANCHALA SREEKANTH <76148957+odysseus1999@users.noreply.github.com>
2022-05-27 14:21:43 +0530

    first commit

---
+++ js/index.js
@@ -0,0 +1,97 @@
+/*
+=============
+Navigation
+=============
+ */
+const navOpen = document.querySelector(".nav__hamburger");
+const navClose = document.querySelector(".close__toggle");
+const menu = document.querySelector(".nav__menu");
+const scrollLink = document.querySelectorAll(".scroll-link");
+const navContainer = document.querySelector(".nav__menu");
+
+navOpen.addEventListener("click", () => {
+  menu.classList.add("open");
+  document.body.classList.add("active");
+  navContainer.style.left = "0";
+  navContainer.style.width = "30rem";
+});
+
+navClose.addEventListener("click", () => {
+  menu.classList.remove("open");
+  document.body.classList.remove("active");
+  navContainer.style.left = "-30rem";
+  navContainer.style.width = "0";
+});
+
+/*
+=============
+PopUp
+=============
+ */
+
+
+/*
+=============
+Fixed Navigation
+=============
+ */
+
+const navBar = document.querySelector(".navigation");
+const gotoTop = document.querySelector(".goto-top");
+
+// Smooth Scroll
+Array.from(scrollLink).map(link => {
+  link.addEventListener("click", e => {
+    // Prevent Default
+    e.preventDefault();
+
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    const navHeight = navBar.getBoundingClientRect().height;
+    const fixNav = navBar.classList.contains("fix__nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixNav) {
+      position = position - navHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    navContainer.style.left = "-30rem";
+    document.body.classList.remove("active");
+  });
+});
+
+// Fix NavBar
+
+window.addEventListener("scroll", e => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navBar.getBoundingClientRect().height;
+  if (scrollHeight > navHeight) {
+    navBar.classList.add("fix__nav");
+  } else {
+    navBar.classList.remove("fix__nav");
+  }
+
+  if (scrollHeight > 300) {
+    gotoTop.classList.add("show-top");
+  } else {
+    gotoTop.classList.remove("show-top");
+  }
+});
+const btn = document.querySelector(".place-order");
+
+btn.addEventListener("click", () => {
+  btn.classList.remove("place-order--default");
+  btn.classList.add("place-order--placing");
+  setTimeout(() => {
+    btn.classList.remove("place-order--placing");
+    btn.classList.add("place-order--done");
+  }, 4000);
+  setTimeout(() => {
+    btn.classList.remove("place-order--done");
+    btn.classList.add("place-order--default");
+  }, 6000);
+})
